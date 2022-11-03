fbadf09d31645fcdfa79b808f30b076bb8ccb6bb
ttdp <tiantong2@126.com>
2022-11-03 15:15:41 +0800

    add router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router } from "react-router-dom"
 
 import Header from "./components/Header";
 import TodoContainer from "./components/TodoContainer"
 
-//stylesheet
 import "./App.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <Header />
-    <TodoContainer />
+    <Router>
+      <Header />
+      <TodoContainer />
+    </Router>
   </React.StrictMode>
 );
